498b61b4d68c438365af3e70ec69cd39b93a40b8
Tom <Tom@Tom.Com>
2013-03-18 20:38:29 +0100

    some sample output in runtime

--- erts/emulator/beam/erl_bif_wangjia.c
+++ erts/emulator/beam/erl_bif_wangjia.c
@@ -52,7 +52,7 @@ BIF_RETTYPE wangjia_hiwangjia_1(BIF_ALIST_1)
 
 /* convert an integer to a list of ascii integers */
 
-/*
+/* cool
 wangjia:integer2list(3456).
 */
 
@@ -68,6 +68,7 @@ BIF_RETTYPE wangjia_integer2list_1(BIF_ALIST_1)
     if (is_small(BIF_ARG_1)) {
 	char *c;
 	int n;
+        Eterm e;
         // MacOS defined(ARCH_64) && !HALFWORD_HEAP
 	struct Sint_buf ibuf;
 
@@ -79,13 +80,47 @@ BIF_RETTYPE wangjia_integer2list_1(BIF_ALIST_1)
         erts_printf("String Length: %d\r\n", n);
 	need = 2*n;
 	hp = HAlloc(BIF_P, need);
-	BIF_RET(buf_to_intlist(&hp, c, n, NIL));
+        e = buf_to_intlist(&hp, c, n, NIL);
+        print_tagged_memory(hp - 10, hp + 10);
+	BIF_RET(e);
     }
     else {
       printf("ERROR, can only handle small integers\r\n");
       BIF_RET(am_false);
     }
 }
+/*
+2> wangjia:integer2list(3456). 
+Small Integer: 3456
+String: 3456
+String Length: 4
++--------------------+--------------------+
+| 0x0000000014064070 - 0x0000000014064108 |
+| Address            | Contents           |
+|--------------------|--------------------|
+| 0x0000000014064070 | 0x000000000000c98b | wangjia
+| 0x0000000014064078 | 0x000000000000ca0b | integer2list
+| 0x0000000014064080 | 0x000000000000036f | 54
+| 0x0000000014064088 | 0xfffffffffffffffb | []
+| 0x0000000014064090 | 0x000000000000035f | 53
+| 0x0000000014064098 | 0x0000000014064081 | "6"
+| 0x00000000140640a0 | 0x000000000000034f | 52
+| 0x00000000140640a8 | 0x0000000014064091 | "56"
+| 0x00000000140640b0 | 0x000000000000033f | 51
+| 0x00000000140640b8 | 0x00000000140640a1 | "456"
+| 0x00000000140640c0 | 0x0000000014063539 | [{dict,53,16,16,8,80,48,{[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]},... 
+| 0x00000000140640c8 | 0x0000000014063171 | [16|16]
+| 0x00000000140640d0 | 0xfffffffffffffffb | []
+| 0x00000000140640d8 | 0x0000000014063e81 | [<cp/header:0x00000000141737c0>
+| 0x00000000140640e0 | 0x0000000014062979 | [49|16]
+| 0x00000000140640e8 | 0x0000000014064079 | [integer2list|54]
+| 0x00000000140640f0 | 0x0000000014062999 | [80|48]
+| 0x00000000140640f8 | 0x00000000140629a9 | [{[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]}|{{[[{boolean... 
+| 0x0000000014064100 | 0x00000000140629b9 | [<cp/header:0x0000000000000080>
+| 0x0000000014064108 | 0xfffffffffffffffb | []
++--------------------+--------------------+
+
+*/
 
 int
 my_list_length_copied_from_utils_c(Eterm list)
@@ -108,9 +143,9 @@ wangjia:mylength([1,2,3]).
 BIF_RETTYPE wangjia_mylength_1(BIF_ALIST_1)
 {
   int i;
-  i = list_length(BIF_ARG_1);
+  i = my_list_length_copied_from_utils_c(BIF_ARG_1);
   printf("Length: %d\r\n", i);
-  BIF_RET(am_false);
+  BIF_RET(make_small(i));
 }
 
 
