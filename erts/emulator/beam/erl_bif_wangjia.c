70f52774b9ec496e75d07f1b232134ff2d29ec22
Tom <Tom@Tom.Com>
2013-03-18 19:59:27 +0100

    more bif functions

--- erts/emulator/beam/erl_bif_wangjia.c
+++ erts/emulator/beam/erl_bif_wangjia.c
@@ -48,3 +48,101 @@ BIF_RETTYPE wangjia_hiwangjia_1(BIF_ALIST_1)
   }
   BIF_RET(am_false);
 }
+
+
+/* convert an integer to a list of ascii integers */
+
+/*
+wangjia:integer2list(3456).
+*/
+
+BIF_RETTYPE wangjia_integer2list_1(BIF_ALIST_1)
+{
+    Eterm* hp;
+    Uint need;
+
+    if (is_not_integer(BIF_ARG_1)) {
+	BIF_ERROR(BIF_P, BADARG);
+    }
+
+    if (is_small(BIF_ARG_1)) {
+	char *c;
+	int n;
+        // MacOS defined(ARCH_64) && !HALFWORD_HEAP
+	struct Sint_buf ibuf;
+
+        erts_printf("Small Integer: %T\r\n", BIF_ARG_1);
+        int val = signed_val(BIF_ARG_1);
+	c = Sint_to_buf(val, &ibuf);
+	n = sys_strlen(c);
+        printf("String: %s\r\n", c);
+        erts_printf("String Length: %d\r\n", n);
+	need = 2*n;
+	hp = HAlloc(BIF_P, need);
+	BIF_RET(buf_to_intlist(&hp, c, n, NIL));
+    }
+    else {
+      printf("ERROR, can only handle small integers\r\n");
+      BIF_RET(am_false);
+    }
+}
+
+int
+my_list_length_copied_from_utils_c(Eterm list)
+{
+    int i = 0;
+
+    while(is_list(list)) {
+	i++;
+	list = CDR(list_val(list));
+    }
+    if (is_not_nil(list)) {
+	return -1;
+    }
+    return i;
+}
+
+/*
+wangjia:mylength([1,2,3]).
+*/
+BIF_RETTYPE wangjia_mylength_1(BIF_ALIST_1)
+{
+  int i;
+  i = list_length(BIF_ARG_1);
+  printf("Length: %d\r\n", i);
+  BIF_RET(am_false);
+}
+
+
+int
+my_print_list(Eterm list)
+{
+    int i = 0;
+
+    while(is_list(list)) {
+      printf("is list\r\n");
+      i++;
+      /* This doesn't work! */
+      // erts_print("Elem: %d :%T\r\n", i, list_val(list));
+      list = CDR(list_val(list));
+    }
+    if (is_not_nil(list)) {
+	return -1;
+    }
+    return i;
+}
+
+
+/*
+wangjia:print_list([1,2,3]).
+*/
+BIF_RETTYPE wangjia_print_list_1(BIF_ALIST_1)
+{
+  int i;
+  printf("my function to print a list\r\n");
+  i = my_print_list(BIF_ARG_1);
+  printf("Length: %d\r\n", i);
+  BIF_RET(am_true);
+}
+
+
